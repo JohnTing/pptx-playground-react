c265e9aeaa14d613fd3adc50cbe3201a2d3c8ce4
JohnTing <jot.johnting@gmail.com>
2022-12-07 16:56:14 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JSZip from 'jszip';
+
+
+
+
+
+
 
 function App() {
+
+  async function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      alert("no file.");
+      return;
+    }
+    console.log('files:', event.target.files);
+    const zip = await JSZip.loadAsync(event.target.files[0]);
+    console.log(zip);
+    const page1 = zip.file("ppt/slideLayouts/slideLayout1.xml")
+
+    const page1string = await page1?.async("string");
+    console.log(page1string);
+
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type={"file"}  onChange={onChange} accept=".pptx" />
+
     </div>
   );
 }
